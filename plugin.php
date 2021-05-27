297581c9102f503f3c6c692173f6190b384ac00b
Stephan Winiker <stephan.winiker@hslu.ch>
2021-05-27 12:08:05 +0200

    Start Update to ILIAS 7

--- plugin.php
+++ plugin.php
@@ -7,8 +7,8 @@ $version = '2.5.2';
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = '6.0.0';
-$ilias_max_version = '6.99';
+$ilias_min_version = '7.0';
+$ilias_max_version = '7.999';
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible      = 'Nadia Ahmad / Michael Jansen / Guido Vollbach';
